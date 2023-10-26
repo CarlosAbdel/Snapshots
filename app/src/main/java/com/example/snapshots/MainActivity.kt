a7a3cb93a5c6d482d8ee914a204548c5b0702940
Carlos Abdel Pacehco Martínez <Pacheco.mc21@gmail.com>
2023-10-26 15:06:29 -0600

    Initial commit

---
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.snapshots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.snapshots.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityMainBinding
+
+    private lateinit var mActiveFragment : Fragment
+    private lateinit var mFragmentManager : FragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav(){
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, profileFragment, profileFragment::class.java.name)
+            .hide(profileFragment).commit()
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, addFragment, addFragment::class.java.name)
+            .hide(addFragment).commit()
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, homeFragment, homeFragment::class.java.name)
+            .commit()
+
+        mBinding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction()
+                        .hide(mActiveFragment)
+                        .show(homeFragment)
+                        .commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+                R.id.action_add -> {
+                    mFragmentManager.beginTransaction()
+                        .hide(mActiveFragment)
+                        .show(addFragment)
+                        .commit()
+                    mActiveFragment = addFragment
+                    true
+                }
+                R.id.action_profile -> {
+                    mFragmentManager.beginTransaction()
+                        .hide(mActiveFragment)
+                        .show(profileFragment)
+                        .commit()
+                    mActiveFragment = profileFragment
+                    true
+                }
+                else -> false
+
+            }
+        }
+
+    }
+}
