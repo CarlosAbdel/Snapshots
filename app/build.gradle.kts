daaf63e67ae6af570ea219f7afd9e6b2fa6dfb85
Carlos Abdel Pacehco Martínez <Pacheco.mc21@gmail.com>
2023-10-27 17:30:22 -0600

    Integracion y configuracion de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 
 }
 
@@ -54,6 +55,19 @@ dependencies {
 
     implementation ("com.github.bumptech.glide:glide:4.14.2")
     implementation("com.github.bumptech.glide:ksp:4.14.2")
+
+    // FirebaseUI for Firebase Realtime Database
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")
+
+    // FirebaseUI for Firebase Auth
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
